0dd5515cd1492e3b4bcf28214b20378f4ce2e602
Lucas Diedrich <lucas.diedrich@hotmail.de>
2023-05-03 22:25:57 -0400

    Added required scan_n_res parameter to silhouette_param_scan

--- ARBOLpy/ARBOL.py
+++ ARBOLpy/ARBOL.py
@@ -366,7 +366,7 @@ def ARBOL(adata, tier: int = 0, cluster: int = 0,
 
         #find optimum cluster resolution by silhouette analysis
         print('starting silhouette analysis')
-        bestres, fig = silhouette_param_scan(adata, res_array=list(np.logspace(min_res_power, max_res_power, 30)))
+        bestres, fig = silhouette_param_scan(adata, res_array=list(np.logspace(min_res_power, max_res_power, 30)), scan_n_res=scan_n_res)
 
         print(f'best resolution by silhouette analysis: {bestres}')
 
